0b41cb5d61a7bb4f493ac116ed782858878d78c0
Erazem Kokot <contact@erazem.eu>
2020-11-13 21:11:01 +0100

    Bump version number to 1.3.1

--- profiledef.sh
+++ profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="anarchy"
-iso_label="ANARCHY130"
+iso_label="ANARCHY131"
 iso_publisher="Anarchy Installer <https://anarchyinstaller.org>"
 iso_application="Anarchy Installer"
-iso_version="1.3.0"
+iso_version="1.3.1"
 install_dir="anarchy"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
